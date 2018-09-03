7ead25bfef01c79459ba9ad4bb9f6d77348d9404
Reuben Ogbonna <reubenogbonna@gmail.com>
2018-09-03 19:33:33 +0000

    complete 'more stuff' exercises in launch school intro to programming book.

---
+++ course-100/intro-to-programming/more-stuff.rb
@@ -0,0 +1,13 @@
+## Question 1 ##
+exercise_1 = ["laboratory", "experiment", "Pans Labyrinth", "elaborate", "polar bear"]
+puts exercise_1.select {|word| word =~ /lab/ }
+
+## Question 2 ##
+# Nothing happens. The block in the method is not exercised. It would be if the method said "block.call".
+
+## Question 3 ##
+# Exception handling allows the programmer to account for potential errors from unexpected user inputs by specifying the program's actions in the case of an error. It changes the flow control without exiting the program completely.
+
+## Question 4 ##
+# Without the ampersand, the method is not expecting a block.
+
